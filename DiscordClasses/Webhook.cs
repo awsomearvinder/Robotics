7022d2d9b9108126c960eaf9593ad5dd1d889f62
awsomearvinder <45527519+awsomearvinder@users.noreply.github.com>
2019-11-15 22:59:31 -0600

    Remove Console.Writeline()

--- DiscordClasses/Webhook.cs
+++ DiscordClasses/Webhook.cs
@@ -22,7 +22,6 @@ namespace New_Folder.DiscordClasses
         private static readonly HttpClient client = new HttpClient();
         public async Task<string> SendWebhook(string url)
         {
-            Console.WriteLine(JsonConvert.SerializeObject(this));
             var SerializedWebhookToSend = new ByteArrayContent(System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this)));
             SerializedWebhookToSend.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             var response = await client.PostAsync(url, SerializedWebhookToSend);
